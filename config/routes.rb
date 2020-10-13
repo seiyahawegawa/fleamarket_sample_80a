e0d7b7a392dbc00faa685ef207a9b1fea8c75925
seiyahawegawa <h-ayiesn_n-1270@docomo.ne.jp>
2020-10-13 21:18:37 +0900

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'categories/index'
   root 'items#index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :items, only: [:index, :show, :new, :edit, :destroy] do
+    #Ajaxで動くアクションのルートを作成
+    collection do
+      get 'get_category_children', defaults: { format: 'json' }
+      get 'get_category_grandchildren', defaults: { format: 'json' }
+    end
+  end
 end
