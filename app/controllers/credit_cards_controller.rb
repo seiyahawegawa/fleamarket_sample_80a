8e07d7c947dbe7b6d3378b37ffff33cbed6503d9
hagishort <hagishort@gmail.com>
2020-10-15 01:22:34 +0900

    クレジットカード登録画面作成

---
+++ app/controllers/credit_cards_controller.rb
@@ -0,0 +1,15 @@
+class CreditCardsController < ApplicationController
+
+  def index
+  end
+  
+  def new
+  end
+
+  def create
+  end
+  
+  def show
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  root 'users#index'
+  root  'credit_cards#new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
+  resources :credit_cards, only: [:index, :new, :create, :show] do
+  end
+    
 end
