e0769857631ac1bd6763a246801e953ffad846b6
Hitomi-y <yhitomi222@gmail.com>
2020-10-15 21:35:30 +0900

    users_registration

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  root 'items#index'
+  devise_for :users
+    root 'items#index'
+  # root 'items#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
