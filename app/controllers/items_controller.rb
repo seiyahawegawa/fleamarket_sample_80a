d7c1bfd6c9f87f680c55cd5d4b98af0b8d2a3781
musashi-kitaura <dmchyd42@gmail.com>
2020-10-18 20:17:18 +0900

    編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   def new
     @item = Item.new
+    @item.item_images.new
     @category_parent =  Category.where("ancestry is null")
   end
 
@@ -33,7 +34,8 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:name, :text, :category_id, :condition_id, :deliverycost_id, :pref_id, :delivery_days_id, :price, images: []).merge(user_id: current_user.id, boughtflg_id:"1")
+    
+    params.require(:item).permit(:item_name,:item_description, :category_id, :conditon_id, :shopping_charges_id,:prefecture_id, :days_to_delivery, :price, item_iamges_attributes:[:iamge]).merge(user_id: current_user.id, boughtflg_id:"1")
   end
 
 end
