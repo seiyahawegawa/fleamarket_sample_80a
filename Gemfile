cce1ecce6c6b158361c92cd11289528272872855
Hitomi-y <yhitomi222@gmail.com>
2020-10-20 23:25:37 +0900

    registration

--- Gemfile
+++ Gemfile
@@ -67,3 +67,4 @@ end
 gem "haml-rails", "~> 2.0"
 
 gem 'devise'
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => {
+    :registrations => 'users/registrations',
+    :sessions => 'users/sessions'   
+  } 
     root 'items#index'
+
+  devise_scope :user do
+    get "sign_in", :to => "users/sessions#new"
+    get "sign_out", :to => "users/sessions#destroy" 
   # root 'items#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  end
 end
