bbc86a0c252d7194b26db8bdd9394bfe3e4b3cd3
seiyahawegawa <h-ayiesn_n-1270@docomo.ne.jp>
2020-10-24 21:08:24 +0900

    commit

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,7 @@ require("@rails/activestorage").start()
 require("channels")
 require("../items_new.js")
 require('jquery')
+require("../image_new.js")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
