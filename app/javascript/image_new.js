01b18cb30f4a8904ef42aca99d99fbfbf5889724
musashi-kitaura <dmchyd42@gmail.com>
2020-10-25 16:23:16 +0900

    複数枚投稿　JS作成

--- app/javascript/image_new.js
+++ app/javascript/image_new.js
@@ -1,41 +1,92 @@
 $(function() {
 
   function filefield(index) {
-    console.log(index)
     const html = `
-        <input class="image" data-index="${index}" id="${index} "type="file" name="item[item_images_attributes][${index}][image]" id="item_item_images_attributes_${index}_image">
-        <div class="iamge_delete-button"><br>
-          <p>削除</p>
-        </div>`;
-    console.log(html)
+        <input class="image" data-index="${index}" id="item_item_images_attributes_${index}_image" type="file" name="item[item_images_attributes][${index}][image]" id="item_item_images_attributes_${index}_image" >
+        `;
     return html;    
   };
   const buildImg = (index, url) => {
-    const html = `<img data-index="${index}" src="${url}" width="100px" height="100px">`;
+    const html = `
+    <div class="image-content">
+      <img data-index="${index}" src="${url}" width="100px" height="100px"class="image-box">
+      <div class="image_delete-button" width="50px" height="50px" >
+            <p class="delete">削除</p> 
+      </div>
+    </div>  
+    `;
     return html;
   }
-
-  let fileIndex = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  const image= (index)=>{
+    const html =`
+    <ul id="output-box-secound">
+      <div id="image-input-secound">
+        <label class="sell-container__content__upload__items__box__label" for="item_item_images_attributes_${index}_image">
+          <pre style= "display: inherit;">
+            <i class="fas fa-camera fa-lg"></i>
+          </pre>
+        </label>
+    </ul>    
+    `;
+    return html;
+    
+  }
   //ここの記述で、インプットボタンを押した時にイベントが起きる
   $(document).on('change', ".image", function(e){
     // $(".image_group").append(image(fileIndex[0]));
     // console.log("ヤッター");
     // fileIndex.shift();
     // fileIndex.push(fileIndex[fileIndex.length - 1] + 1);
+  
+    var ImageContent = $(".image-content").length 
+    console.log(ImageContent)
     var index = $(this).data("index")
     // console.log(index)
     index += 1;
     // console.log(index)
     const file_field = filefield(index);
-    $(".image_group").append(file_field)
-      const file = e.target.files[0];
-      const blobUrl = window.URL.createObjectURL(file);
-      console.log(blobUrl)
+    const file = e.target.files[0];
+    const blobUrl = window.URL.createObjectURL(file);
+    // $("#output-box").prepend(buildImg(index, blobUrl));
+    // $(".image_group").append(file_field)
+    // $('.sell-container__content__upload__items__box__label').attr("for", `item_item_images_attributes_${index}_image`);
+    if (ImageContent<=3){
+      $("#output-box").prepend(buildImg(index, blobUrl));
+      $(".image_group").append(file_field)
+      $('.sell-container__content__upload__items__box__label').attr("for", `item_item_images_attributes_${index}_image`);
+      $(".drop-and-drag").text("")
+    } 
+    else if (ImageContent==4){
+      $('#image-input').hide()
+      $('#output-box').append(image(index))
+      console.log('ヤッター')
+      $("#output-box").prepend(buildImg(index, blobUrl));
+      $(".image_group").append(file_field)
+      $('.sell-container__content__upload__items__box__label').attr("for", `item_item_images_attributes_${index}_image`);
+      $(".drop-and-drag").text("")
+      console.log("やったー")
+    } else if (ImageContent<=8){
+      $("#output-box-secound").prepend(buildImg(index, blobUrl));
+      $(".image_group").append(file_field)
+      $('.sell-container__content__upload__items__box__label').attr("for", `item_item_images_attributes_${index}_image`);
+      $(".drop-and-drag").text("")
+    } else if (ImageContent ==9){
+      console.log('最高')
+      $("#image-input-secound").hide()
+      $("#output-box-secound").prepend(buildImg(index, blobUrl));
+      $(".image_group").append(file_field)
+      $('.sell-container__content__upload__items__box__label').attr("for", `item_item_images_attributes_${index}_image`);
+    }
   })
-  $('.image_group').on('click', '.iamge_delete-button',function () {
+  $(document).on('click', ".image_delete-button",function () {
     $(this).parent().remove();
-    // 画像入力欄が0個にならないようにしておく
-    if ($('.image').length == 0) $('.sell-container__content__upload__items__box__label ').append(image(fileIndex[0]));
+    var ImageContent = $(".image-content").length 
+    if (ImageContent==9){
+      $("#image-input-secound").show()
+    }else if (ImageContent==4){
+      $("#image-input-secound").hide()
+      $("#image-input").show()
+      $("#output-box-secound").remove()
+    }
   });
-
 });
