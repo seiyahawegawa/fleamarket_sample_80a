aff0726a3beb28ae1f2774f56af20bf692d066e5
seiyahawegawa <h-ayiesn_n-1270@docomo.ne.jp>
2020-10-25 17:27:23 +0900

    commit

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,11 +4,13 @@ class CategoriesController < ApplicationController
   end
 
   def show
-    @items = @category.set_items
-    @items = @items.where(buyer_id: nil).order("created_at DESC").page(params[:page]).per(9)
+    # @items = @category.set_items
+    # @items = @items.where(buyer_id: nil).order("created_at DESC").page(params[:page]).per(9)
   end
 
   private
-def set_category
+ def set_category
   @category = Category.find(params[:id])
+ end
+
 end
