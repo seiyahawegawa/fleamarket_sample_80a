06e0fa8ac6c0f75db502e52774e46ab5eafa0c9b
musashi-kitaura <dmchyd42@gmail.com>
2020-10-30 18:51:41 +0900

    商品一覧ページのサーバーサイド

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,8 @@
 class ItemsController < ApplicationController
   before_action :set_item, only: [:show,:destroy]
+  def index
+    @item = Item.all
+  end  
   def new
     @item = Item.new
     @item.item_images.new
