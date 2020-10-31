c2536d61fdf51bd3c8f9214a7464c9c698a970ee
hagishort <hagishort@gmail.com>
2020-10-31 13:47:57 +0900

    送付先住所作成中

--- config/routes.rb
+++ config/routes.rb
@@ -11,9 +11,15 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   end
 
+  devise_scope :user do
+    get 'addresses', to: 'users/registrations#new_address'
+    post 'addresses', to: 'users/registrations#create_address'
+  end
+
   root 'items#index'
   resources :credit_cards, only: [:index, :new, :create, :show] 
   resources :logouts, only: [:index]
+  resources :users, only: [:index]
   resources :categories, only: :index
   resources :items, only: [:index, :show, :new, :edit, :destroy, :create] do
     #Ajaxで動くアクションのルートを作成
