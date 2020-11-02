3ff23b721910397b0860807c941942c9726a3865
hagishort <hagishort@gmail.com>
2020-11-02 23:09:21 +0900

    商品送付先住所情報作成（viewとmodel）

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,5 +1,5 @@
 class Address < ApplicationRecord
-  belongs_to :user, optional :true
+  belongs_to :user, optional: true
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
