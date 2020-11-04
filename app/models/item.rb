43c48f06a14bc5d19edf997e7472b9aa28278480
seiyahawegawa <h-ayiesn_n-1270@docomo.ne.jp>
2020-11-04 13:43:48 +0900

    commit

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,10 +1,31 @@
 class Item < ApplicationRecord
-  belongs_to :category
-  has_many :item_images
-  accepts_nested_attributes_for :item_images, allow_destroy: true
+
+  
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
   belongs_to_active_hash :conditon
   belongs_to_active_hash :shopping_charges
   belongs_to_active_hash :days_to_delivery
+
+  validate :images_presence
+  validates :item_name, :item_description, :category_id, :condition_id, :shopping_charges_id, :prefecture_id, :days_to_delivery_id, :boughtflg_id,:user_id, presence: true
+
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+
+  has_many :item_images
+  accepts_nested_attributes_for :item_images, allow_destroy: true
+  belongs_to :user, foreign_key: 'user_id'
+  belongs_to :category
+  
+
+  def images_presence
+    if images.attached?
+      if images.length > 10
+        errors.add(:image, '10枚まで投稿できます')
+      end
+    else
+      errors.add(:image, '画像がありません')
+    end
+  end
+  
 end
---
+++ factory/items.rb
@@ -0,0 +1,36 @@
+FactoryBot.define do
+  factory :item do
+    item_name             {"hoge"}
+    item_description             {Faker::Lorem.sentence}
+    condition_id     {1}
+    shopping_charges_id  {1}
+    prefecture_id          {13}
+    days_to_delivery_id {1}
+    price            {"9999"}
+    boughtflg_id     {1}
+
+    # アソシエーション
+    user
+    category
+
+    # 1枚の画像をアップロードする
+    trait :item_image do
+      after(:build) do |item|
+        File.open("#{Rails.root}/spec/images/Unknown.jpeg") do |f|
+          item.images.attach(io: f, filename: "Unknown.jpeg", content_type: 'image/jpeg')
+        end
+      end
+    end
+
+    # 11枚の画像をアップロードする
+    trait :images do
+      after(:build) do |item|
+        11.times do
+          File.open("#{Rails.root}/spec/images/Unknown.jpeg") do |f|
+            item.images.attach(io: f, filename: "Unknown.jpeg", content_type: 'image/jpeg')
+          end
+        end
+      end
+    end
+  end
+end
