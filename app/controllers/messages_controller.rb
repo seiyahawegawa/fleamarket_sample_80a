1d801c7f1aa8719dc297694d6b2f08373ddd66ac
musashi-kitaura <dmchyd42@gmail.com>
2020-11-11 19:06:31 +0900

    メッセージ機能編集

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,15 +1,27 @@
 class MessagesController < ApplicationController
-  before_action :set_item
-  def new
-  end
-
+  before_action :set_item,except:[:destroy]
   def create
     @message = @item.messages.new(message_params)
-    @item =@message.item
     if @message.save
       redirect_to @item
     end
   end
+
+  def destroy
+    # dependent: :destroy
+    # binding.pry
+    # @item.valid?
+    # @item.errors.full_messages
+    # binding.pry
+    @item = Item.find(params[:id])
+    @message = Message.find(params[:item_id])
+    if @message.destroy
+      redirect_to @item, notice: '削除しました'
+      # redirect_to new_item_message_path(@item), notice: '削除しました'
+    else  
+      redirect_to root_path
+    end
+  end  
   private
   def message_params
     params.require(:message).permit(:message,:item_id).merge(user_id: current_user.id)
