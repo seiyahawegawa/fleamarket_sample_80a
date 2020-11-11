0b7bfe7106bb50d744b3b0ddc65ffb97b13c27ee
musashi-kitaura <dmchyd42@gmail.com>
2020-11-11 20:26:12 +0900

    コメント機能を実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,32 +1,21 @@
 class MessagesController < ApplicationController
-  before_action :set_item,except:[:destroy]
   def create
-    @message = @item.messages.new(message_params)
+    @message = Message.new(message_params)
     if @message.save
-      redirect_to @item
+      redirect_to "/items/#{@message.item.id}"
     end
   end
 
   def destroy
-    # dependent: :destroy
-    # binding.pry
-    # @item.valid?
-    # @item.errors.full_messages
-    # binding.pry
-    @item = Item.find(params[:id])
     @message = Message.find(params[:item_id])
     if @message.destroy
-      redirect_to @item, notice: '削除しました'
-      # redirect_to new_item_message_path(@item), notice: '削除しました'
+      redirect_to "/items/#{@message.item.id}", notice: '削除しました'
     else  
       redirect_to root_path
     end
   end  
   private
   def message_params
-    params.require(:message).permit(:message,:item_id).merge(user_id: current_user.id)
-  end
-  def set_item
-    @item = Item.find(params[:item_id])
+    params.require(:message).permit(:message).merge(user_id: current_user.id,item_id:params[:item_id])
   end
 end
