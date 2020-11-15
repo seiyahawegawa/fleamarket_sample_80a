b49e1edfe2db30b57ff80509bd690e9ee7b191a8
makisouu <makisouu@icloud.com>
2020-11-15 14:18:39 +0900

    編集4

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,11 +7,11 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
     @item.item_images.new
-    @category_parent =  Category.where("ancestry is null")
+    @category_parent = Category.where("ancestry is null")
   end
 
   def show
-    @message =Message.new
+    @message = Message.new
   end  
   # 親カテゴリーが選択された後に動くアクション
   def category_children
@@ -46,14 +46,17 @@ class ItemsController < ApplicationController
 
   def edit
     # エラー防止の為仮置でインスタンス変数を定義
-    @category_parent =  Category.where("ancestry is null")
+    @category_parent = Category.where("ancestry is null")
     @item = Item.find(params[:id])
+    @item.item_images.new
   end
 
   def update
+    @category_parent = Category.where("ancestry is null")
     @item = Item.find(params[:id])
+    # binding.pry
     if @item.update(item_params)
-      redirect_to root_path
+      redirect_to root_path, notice: '更新しました'
     else
       render :edit
     end
@@ -62,7 +65,7 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:item_name,:item_description, :category_id, :conditon_id, :shopping_charges_id,:prefecture_id, :days_to_delivery_id, :price, item_images_attributes:[:image]).merge(user_id: 1)
+    params.require(:item).permit(:item_name,:item_description, :category_id, :conditon_id, :shopping_charges_id,:prefecture_id, :days_to_delivery_id, :price, item_images_attributes:[:image, :id, :_destroy]).merge(user_id: 1)
   end
   # .merge(user_id: current_user.id, boughtflg_id:"1")
   def set_item 
--- app/javascript/image_new.js
+++ app/javascript/image_new.js
@@ -8,8 +8,8 @@ $(function() {
   };
   const buildImg = (index, url) => {
     const html = `
-    <div class="image-content">
-      <img data-index="${index}" src="${url}" width="100px" height="100px"class="image-box">
+    <div class="image-content" data-index="${index}">
+      <img src="${url}" width="100px" height="100px"class="image-box">
       <div class="image_delete-button" width="50px" height="50px" >
             <p class="delete">削除</p> 
       </div>
@@ -31,16 +31,27 @@ $(function() {
     return html;
     
   }
+
+  // $("#select_image").on("click", function() {
+  //   const file_field = $(".image:last");
+  //   file_field.trigger("click")
+  // })
+
   //ここの記述で、インプットボタンを押した時にイベントが起きる
   $(document).on('change', ".image", function(e){
-    var ImageContent = $(".image-content").length 
+    var ImageContent = $(".image-content").length
+
     var index = $(this).data("index")
+  
+  
     index += 1;
+    console.log(index)
     const file_field = filefield(index);
     const file = e.target.files[0];
     const blobUrl = window.URL.createObjectURL(file);
     if (ImageContent<=3){
-      $("#output-box").prepend(buildImg(index, blobUrl));
+      $("#output-box").prepend(buildImg(ImageContent, blobUrl));
+      // $("#output-box").prepend(buildImg(index, blobUrl));
       $(".image_group").append(file_field)
       $('.sell-container__content__upload__items__box__label').attr("for", `item_item_images_attributes_${index}_image`);
       $(".drop-and-drag").text("")
@@ -74,5 +85,11 @@ $(function() {
       $("#image-input").show()
       $("#output-box-secound").remove()
     }
+    const targetIndex = $(this).parent().data('index')
+    // console.log(targetIndex)
+    // 該当indexを振られているチェックボックスを取得する
+    const hiddenCheck = $(`input[data-index="${targetIndex}"].hidden-destroy`);
+    // // // もしチェックボックスが存在すればチェックを入れる
+    if (hiddenCheck) hiddenCheck.prop('checked', true);
   });
 });
