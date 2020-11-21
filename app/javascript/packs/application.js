230ef4a6b768ae2d99b1809693252a25a1cae942
makisouu <makisouu@icloud.com>
2020-11-21 11:46:07 +0900

    編集

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,7 +11,6 @@ require("../items_new.js")
 require("../image_new.js")
 require("../payjp.js")
 require('jquery')
-require("../edit.js")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
