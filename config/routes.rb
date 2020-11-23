14a3ced13ac35944b4f900576fc17a0eaf80fe8e
hagishort <hagishort@gmail.com>
2020-11-23 19:56:29 +0900

    コミット

--- config/routes.rb
+++ config/routes.rb
@@ -21,7 +21,7 @@ Rails.application.routes.draw do
   end
 
   root 'items#index'
-  resources :credit_cards, only: [:index, :new, :create, :show] 
+  resources :credit_cards, only: [:index, :new, :create, :show, :destroy] 
   resources :logouts, only: [:index]
   resources :users, only: [:index]
   resources :categories, only: :index
